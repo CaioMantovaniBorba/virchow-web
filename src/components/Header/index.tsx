93eac9efa36e1bbf400f3b65d11cd8aa10add753
Caio Mantovani Borba <caiovmborba@gmail.com>
2025-05-12 09:21:50 -0300

    feat: active tab

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,29 +15,47 @@ function Header() {
   const userString = localStorage.getItem("user");
   const user = userString ? JSON.parse(userString) : null;
 
+  const baseStyle =
+    "text-white text-center font-bold w-[250px] px-12 py-1.5 rounded-sm max-md:text-xs";
+  const location = useLocation();
+
+  const isPacienteRoute = () =>
+    ["/cadastropaciente", "/cadastrarpaciente", "/editarpaciente"].includes(location.pathname);
+
+  const isLaudoRoute = () =>
+    ["/incluirlaudo", "/pedidolaudo"].includes(location.pathname);
+
   return (
     <div className="bg-[#0F6278] absolute top-0 left-0 w-full h-[60px] flex items-center justify-between">
       <img src={Virchow} alt="Logo Virchow" className="h-[30px] ml-8" />
 
       <ul className="flex space-x-8">
-        {/* <NavLink
+        <NavLink
           to="/"
-          className="bg-[#0C647C] text-white text-center font-bold w-[250px] px-12 py-1.5 rounded-sm max-md:text-xs"
+          className={() =>
+            `${isLaudoRoute() ? "bg-[#197791]/50" : "bg-[#197791]"} ${baseStyle}`
+          }
         >
           INCLUIR LAUDO
-        </NavLink> */}
+        </NavLink>
+
         <NavLink
           to="/cadastropaciente"
-          className="bg-[#197791] text-white text-center font-bold w-[250px] px-12 py-1.5 rounded-sm max-md:text-xs"
+          className={() =>
+            `${isPacienteRoute() ? "bg-[#197791]/50" : "bg-[#197791]"} ${baseStyle}`
+          }
         >
           PACIENTES
         </NavLink>
-        {/* <NavLink
+
+        <NavLink
           to="/impressoes"
-          className="bg-[#197791] text-white text-center font-bold w-[250px] px-12 py-1.5 rounded-sm max-md:text-xs"
+          className={({ isActive }) =>
+            `${isActive ? "bg-[#197791]/50" : "bg-[#197791]"} ${baseStyle}`
+          }
         >
           IMPRESSOS
-        </NavLink> */}
+        </NavLink>
       </ul>
 
       <span className="text-white hover:cursor-pointer mr-8" onClick={() => handleLogout()}>SAIR</span>
